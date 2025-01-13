535f1a46a3179c37d90f8bed8c28d8fbd4f9fea2
Lisa Johansson <109794046+LisaVasic@users.noreply.github.com>
2025-01-13 10:01:11 +0100

    Update Tech.js

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -6,9 +6,10 @@ export const Tech = () => {
     <TechOuterWrapper>
       <TechInnerWrapper>
         <h5>TECH</h5>
-        <p>HTML, CSS, Flexbox, Tailwind <span>JavaScript</span>, TypeScript, 
+        <p>HTML, CSS, Flexbox, Tailwind <span>JavaScript</span>, TypeScript,
           <span> React, React Hooks, Redux, </span>
-        Next.js, Node.js, MongoDB, Prisma, SQL, Git, Web Accessibly, API:s, mob-programming, pair-programming.
+        Next.js, Node.js, MongoDB, Prisma, SQL, Git, Web Accessibly, 
+        API:s, mob-programming, pair-programming.
         </p>
       </TechInnerWrapper>
     </TechOuterWrapper>
